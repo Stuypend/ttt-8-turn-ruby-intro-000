d22a25ede2ad64aef2afc54aca017f4243a893ea
Andre Powers <andre@stuypend.com>
2021-01-25 14:43:50 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,9 @@ require_relative '../lib/display_board'
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn(board)
-  index = -2
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
   until valid_move?(board, index)
     puts "Please enter 1-9:"
     input = gets.strip
